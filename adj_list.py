36100d9b5866e671e0e35a3a5a70c31a19db3922
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-10 13:07:42 +0100

    day 1 prep

---
+++ adj_list.py
@@ -0,0 +1,28 @@
+from queue import Queue
+from stack import Stack
+# lets do a graph adjacency list
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        pass
+
+    def add_vertex(self, vertex_id):
+        pass
+
+    def add_edge(self, v1, v2):
+        pass
+
+    def get_neighbors(self, vertex_id):
+        pass
+
+    def bft(self, starting_vertex_id):
+        pass
+
+    def dft(self, starting_vertex_id):
+        pass
+    
+    def bfs(self, starting_vertex_id, target_vertex_id):
+        pass
+
+    def dfs(self, starting_vertex_id, target_vertex_id):
+        pass
