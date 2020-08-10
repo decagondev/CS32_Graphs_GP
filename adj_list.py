b689deddc877fbbcf2964e5f7665ae630dffd899
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-10 17:45:28 +0100

    Part 1 (What is a graph)

--- adj_list.py
+++ adj_list.py
@@ -4,18 +4,33 @@ from stack import Stack
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass
+        self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        pass
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        pass
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex Does not exist!")
 
     def get_neighbors(self, vertex_id):
-        pass
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex_id):
+        # create an empty queue and enqueue a starting vertex
+
+        # create a set to store the visited vertices
+
+        # while the queue is not empty
+            # dequeue the first vertex
+
+            # if vertex has not been visited
+                # mark the vertex as visited
+                # print it for debug
+
+                # add all of it's neighbors to the back of the queue
         pass
 
     def dft(self, starting_vertex_id):
@@ -26,3 +41,4 @@ class Graph:
 
     def dfs(self, starting_vertex_id, target_vertex_id):
         pass
+    
